ca7f9f33903623491c710e0d55694927775bc82f
Ranjit Singh <ranjitkamboz302@gmail.com>
2022-03-13 22:21:22 +0530

    Add files

---
+++ src/com/brigelabz/StartPosition0.java
@@ -0,0 +1,8 @@
+package com.brigelabz;
+
+public class StartPosition0 {
+    public static void main(String[] args) {
+        int pos = 0;
+        System.out.println("Single player at start position "+ pos);
+    }
+}
